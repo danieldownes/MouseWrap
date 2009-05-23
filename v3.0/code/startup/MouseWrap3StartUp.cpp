e70578a4957d8229e9dd8f8fe53a85363c16d507
Daniel Downes <dd@danieldownes.co.uk>
2009-05-23 14:14:14 +0700

    ./v3.0/code/startup/MouseWrap3StartUp.cpp

---
+++ v3.0/code/startup/MouseWrap3StartUp.cpp
@@ -0,0 +1,77 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+//---------------------------------------------------------------------------
+USEFORM("Unit1.cpp", frmMain);
+//---------------------------------------------------------------------------
+WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR cmd, int)
+{
+	try
+	{
+                char *ReadReg(char *sKey);
+
+                // Exit if we are starting from Windows login, but the user dosn't want
+                //  MW to load on Windows login
+        	if(!strcmp(cmd,"/start") && !strcmp(ReadReg("OnStartUp"), "0"))
+	        {
+        	        exit(0);
+	        }
+
+		Application->Initialize();
+		Application->Title = "MouseWrap3Start";
+                 Application->CreateForm(__classid(TfrmMain), &frmMain);
+                 Application->Run();
+	}
+	catch (Exception &exception)
+	{
+		Application->ShowException(&exception);
+	}
+	catch (...)
+	{
+		try
+		{
+			throw Exception("");
+		}
+		catch (Exception &exception)
+		{
+			Application->ShowException(&exception);
+		}
+	}
+	return 0;
+}
+//---------------------------------------------------------------------------
+
+char *ReadReg(char *sKey)
+{
+	HKEY hk;
+	DWORD dwData;
+	UCHAR szBuf[80];
+
+	/*
+	 * Add your source name as a subkey under the Application
+	 * key in the EventLog service portion of the registry.
+	 */
+
+	if(RegCreateKey(HKEY_CURRENT_USER, "Software\\QDwares\\MouseWrap3",&hk))
+		Application->MessageBox("could not open registry key","Mouse Wrap 3 - Error: RegRead");
+
+
+	/* Add the Event ID message-file name to the subkey. */
+	DWORD Size;
+	Size = 80;
+
+	RegQueryValueEx(hk,             	/* subkey handle         */
+		sKey,                   		/* value name            */
+		NULL,                        	/* must be zero          */
+		NULL,            				/* value type            */
+		(LPBYTE) szBuf,           		/* address of value data */
+		&Size);       					/* length of value data  */
+
+
+	RegCloseKey(hk);
+
+//	Application->MessageBoxA(szBuf,"Mouse Wrap 3 - Error: RegRead");
+
+	return szBuf;
+}
